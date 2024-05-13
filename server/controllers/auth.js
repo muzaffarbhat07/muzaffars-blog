422c617d8d99d102490efef68b10c71ec55c750b
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-05-14 00:36:09 +0530

    auth: sigin api

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,19 +1,12 @@
 import User from '../models/user.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import ExpressError from '../utils/ExpressError.js';
 import catchAsync from '../utils/catchAsync.js';
 
 export const signup = catchAsync(async (req, res, next) => {
   const { username, email, password } = req.body;
-
-  if (
-    !username ||
-    !email ||
-    !password ||
-    username === '' ||
-    email === '' ||
-    password === ''
-  ) {
+  if (!username || !email || !password) {
     throw new ExpressError('All fields are required', 400);
   }
   const userExists = await User.findOne({
@@ -32,4 +25,30 @@ export const signup = catchAsync(async (req, res, next) => {
 
   await newUser.save();
   res.json('Signup successful');
+});
+
+export const signin = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new ExpressError('All fields are required', 400);
+  }
+  
+  const validUser = await User.findOne({ email });
+  if (!validUser) {
+    throw new ExpressError('User not found', 404);
+  }
+  const validPassword = await bcrypt.compare(password, validUser.password);
+  if (!validPassword) {
+    throw new ExpressError('Invalid password', 400);
+  }
+  const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+  const { password: pass, ...rest } = validUser._doc;
+
+  res
+    .status(200)
+    .cookie('access_token', token, {
+      httpOnly: true,
+    })
+    .json(rest);
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { signup } from '../controllers/auth.js';
+import { signup, signin } from '../controllers/auth.js';
 
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 export default router;
