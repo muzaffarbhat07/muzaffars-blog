351339d6ec18514232c375bf5ae8ea29d2a3cec7
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-05-14 23:56:02 +0530

    auth: google signin api

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -52,3 +52,43 @@ export const signin = catchAsync(async (req, res, next) => {
     })
     .json(rest);
 });
+
+export const google = catchAsync(async (req, res, next) => {
+  const { email, name, googlePhotoUrl } = req.body;
+  if (!email || !name || !googlePhotoUrl) {
+    throw new ExpressError('All fields are required', 400);
+  }
+  const user = await User.findOne({ email });
+  if (user) {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password, ...rest } = user._doc;
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } else {
+    const generatedPassword =
+      Math.random().toString(36).slice(-8) +
+      Math.random().toString(36).slice(-8);
+    const hashedPassword = await bcrypt.hash(generatedPassword, 10);
+    const newUser = new User({
+      username:
+        name.toLowerCase().split(' ').join('') +
+        Math.random().toString(9).slice(-4),
+      email,
+      password: hashedPassword,
+      profilePicture: googlePhotoUrl,
+    });
+    await newUser.save();
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+    const { password, ...rest } = newUser._doc;
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  }
+});
