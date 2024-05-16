fc0936f3ff53019d7913cf821a19099a907df9c7
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-05-17 00:02:21 +0530

    add create post page UI with admin only protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
+import CreatePost from './pages/CreatePost';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 
 function App() {
   return (
@@ -25,6 +27,11 @@ function App() {
         {/* Private Routes */}
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+
+        {/* Only Admin Routes */}
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
         </Route>
       </Routes>
       <Footer />
