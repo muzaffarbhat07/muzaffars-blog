4afe9b7d46ac65e304c1872e33e5d95d7fa29c7e
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-05-16 11:50:20 +0530

    add PrivateRoute component to protect private routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -14,12 +15,17 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
-        <Route path="/dashboard" element={<Dashboard />}/>
         <Route path="/projects" element={<Projects />}/>
         <Route path="/signin" element={<SignIn />}/>
         <Route path="/signup" element={<SignUp />}/>
+
+        {/* Private Routes */}
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
