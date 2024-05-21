becf06bfdf6f7cf276d74f2aceb5bba1315df682
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-05-22 00:26:22 +0530

    complete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import PrivateRoute from './components/PrivateRoute';
@@ -22,6 +23,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/projects" element={<Projects />}/>
+        <Route path='/post/:postSlug' element={<PostPage />} />
         <Route path="/signin" element={<SignIn />}/>
         <Route path="/signup" element={<SignUp />}/>
 
