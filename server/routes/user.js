7fd040eabbebb50609f493e2ec9d046b1e7e1ca5
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-05-21 23:55:53 +0530

    add getUsers api

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 const router = Router();
-import { test, updateUser, deleteUser } from '../controllers/user.js';
+import { test, updateUser, deleteUser, getUsers } from '../controllers/user.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
+router.get('/getusers', verifyToken, getUsers);
 
 export default router;
